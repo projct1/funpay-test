f15cfa3db2953ff4409f51349db64ed7a33abc5d
osipovmn <kindly1987@gmail.com>
2024-04-29 20:14:25 +0300

    разбил код на методы в классе Database

--- Database/Database.php
+++ Database/Database.php
@@ -4,6 +4,7 @@ namespace FpDbTest\Database;
 
 use FpDbTest\Contracts\DatabaseInterface;
 use FpDbTest\Enums\CustomQueryParamModifiersEnum;
+use FpDbTest\QueryMatchers\QueryMatchItem;
 use FpDbTest\QueryMatchers\QueryMatchManager;
 use InvalidArgumentException;
 use mysqli;
@@ -20,35 +21,14 @@ class Database implements DatabaseInterface
     public function buildQuery(string $query, array $args = []): string
     {
         if (preg_match_all(CustomQueryParamModifiersEnum::regexp(), $query, $matches, PREG_OFFSET_CAPTURE)) {
-            if (($cntMatches = substr_count($query, '?')) !== $cntArgs = count($args)) {
-                throw new InvalidArgumentException(
-                    "Количество спецификаторов ($cntMatches) в запросе « $query » не совпадает с количеством переданных значений ($cntArgs)"
-                );
-            }
+            $this->guessError($query, $args);
 
             $manager = new QueryMatchManager($matches, $args);
 
             $manager->setQuery($query);
 
-            foreach ($manager->getMatches() as $index => $item) {
-                if ($item->isConditionalBlock()) { //если попался условный блок
-                    /*
-                     * Если нет спецификаторов, то какой смысл от условного блока?
-                     * Скорее всего ошибка.
-                     * Можно было его просто удалить, но, скорее всего, забыли добавить или где-то на уровень выше произошла ошибка.
-                     */
-                    if ($item->hasntSpecifier()) {
-                        throw new InvalidArgumentException("Условный блок $item->match не имеет спецификаторов");
-                    }
-
-                    if ($item->shouldntSkip($this->skip())) { //если переданное значение не нужно пропускать
-                        $item->setValues([$this->buildQuery($item->group, $item->values)]); //парсим отдельно условный блок
-                    }
-                } else { //обрабатываем обычные спецификаторы
-                    $item->setValues(
-                        [CustomQueryParamModifiersEnum::from($item->match)->transform($item->values[0], $this->mysqli)]
-                    );
-                }
+            foreach ($manager->getMatches() as $item) {
+                $item->isConditionalBlock() ? $this->processBlock($item) : $this->processSpecifier($item);
 
                 $manager->replaceMatch($item);
             }
@@ -57,6 +37,38 @@ class Database implements DatabaseInterface
         }
 
         return $query;
+    }
+
+    protected function guessError(string $query, array $args): void
+    {
+        if (($cntMatches = substr_count($query, '?')) !== $cntArgs = count($args)) {
+            throw new InvalidArgumentException(
+                "Количество спецификаторов ($cntMatches) в запросе « $query » не совпадает с количеством переданных значений ($cntArgs)"
+            );
+        }
+    }
+
+    protected function processSpecifier(QueryMatchItem $item): void
+    {
+        $item->setValues(
+            [CustomQueryParamModifiersEnum::from($item->match)->transform($item->values[0], $this->mysqli)]
+        );
+    }
+
+    protected function processBlock(QueryMatchItem $item): void
+    {
+        /**
+         * Если нет спецификаторов, то смысла от условного блока нет.
+         * Скорее всего ошибка.
+         * Можно было его просто удалить, но, скорее всего, забыли добавить спецификатор, либо где-то на уровень выше ошибка.
+         */
+        if ($item->hasntSpecifier()) {
+            throw new InvalidArgumentException("Условный блок $item->match не имеет спецификаторов");
+        }
+
+        if ($item->shouldntSkip($this->skip())) { //если переданное значение не нужно пропускать
+            $item->setValues([$this->buildQuery($item->group, $item->values)]); //парсим отдельно условный блок
+        }
     }
 
     public function skip()
